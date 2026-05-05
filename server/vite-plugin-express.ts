9fc090e0322e26d09842be613eab176892dd45b1
黄敏坚 <>
2026-05-05 16:14:31 +0800

    修改

--- server/vite-plugin-express.ts
+++ server/vite-plugin-express.ts
@@ -4,6 +4,10 @@ import app from './index'
 export function viteExpressPlugin() {
   return {
     name: 'vite-express-plugin',
+    
+    // 👇 加这个判断：只在开发环境执行
+    apply: 'serve',
+
     configureServer(server: any) {
       server.middlewares.use(app as Express)
     },
